0881a754b16a5b42a6bcd8148bf6aa87e4dea0c9
Freena Wang <freena22@gmail.com>
2017-07-26 22:19:00 -0700

    add parse excel file more practice

--- Data-Wrangling/02_Parse_xlrd.py
+++ Data-Wrangling/02_Parse_xlrd.py
@@ -46,3 +46,46 @@ def parse_file(datafile):
 
 parse_file(datafile)
 
+print('++++++++++')
+"""
+Your task is as follows:
+- read the provided Excel file
+- find and return the min, max and average values for the COAST region(Col1)
+- find and return the time value for the min and max entries
+- the time values should be returned as Python tuples
+
+Please see the test function for the expected return format
+
+"""
+def parse_file2(datafile):
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+    data = [[sheet.cell_value(r, col) 
+                for col in range(sheet.ncols)]
+                    for r in range(sheet.nrows)]
+    cv = sheet.col_values(1,start_rowx=1, end_rowx=None)
+
+    maxval = max(cv)
+    minval = min(cv)
+
+    maxpos = cv.index(maxval) + 1
+    minpos = cv.index(minval) + 1
+
+    maxtime = sheet.cell_value(maxpos,0)
+    realtime = xlrd.xldate_as_tuple(maxtime,0)
+    mintime = sheet.cell_value(minpos,0)
+    realmintime = xlrd.xldate_as_tuple(mintime,0)
+
+    data = {
+        'maxtime':realtime,
+        'maxvalue': maxval,
+        'mintime': realmintime,
+        'minvalue': minval,
+        'avgcoast': sum(cv)/float(len(cv))
+        }
+    return data
+
+data = parse_file2(datafile)
+import pprint
+pprint.pprint(data)
+
