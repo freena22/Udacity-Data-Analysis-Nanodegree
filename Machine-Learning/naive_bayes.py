bc4ec22fe525594a89ae229c6ecc24f03362d256
Freena Wang <freena22@gmail.com>
2017-07-28 21:55:54 -0700

    naive bayes basic

---
+++ Machine-Learning/naive_bayes.py
@@ -0,0 +1,22 @@
+
+### sklearn.naive_bayes.GaussianNB
+
+# official example
+
+import numpy as np
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+Y = np.array([1, 1, 1, 2, 2, 2])
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(X, Y)
+print(clf.predict([[-0.8, -1]])) # -> [1]
+print(clf.predict([1, 2])) # -> [2]
+
+
+
+from sklearn.metrics import accuracy_score
+x = [1, 1, 1, 4, 3, 5]
+y = [1, 1, 1, 2, 2, 2]
+print(accuracy_score(x,y))
+# -> 0.5
+
