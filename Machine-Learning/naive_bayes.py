d7a3cc39b25066fd4a26bd13d0b7e0f2df8f179c
freena22 <freena22@gmail.com>
2017-07-29 22:33:56 -0700

    correct the version

--- Machine-Learning/naive_bayes.py
+++ Machine-Learning/naive_bayes.py
@@ -20,3 +20,23 @@ y = [1, 1, 1, 2, 2, 2]
 print(accuracy_score(x,y))
 # -> 0.5
 
+"""
+This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+Use a Naive Bayes Classifier to identify emails by their authors authors and labels:
+Sara has label 0
+Chris has label 1
+"""
+  
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+features_train, features_test, labels_train, labels_test = preprocess()
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+print(pred) #-> [001]
+print(accuracy_score(pred,labels_test))
+# -> 0.973265
