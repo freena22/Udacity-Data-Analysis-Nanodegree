961198939ddbff250f2711a54d3bd721c3a4f686
freena22 <freena22@gmail.com>
2017-08-05 00:12:11 -0700

    TfidfVectorizer

---
+++ Machine-Learning/vectorize_text.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+import os
+import pickle
+import re
+import sys
+
+sys.path.append( "../tools/" )
+from parse_out_email_text import parseOutText
+
+"""
+    Starter code to process the emails from Sara and Chris to extract
+    the features and get the documents ready for classification.
+
+    The list of all the emails from Sara are in the from_sara list
+    likewise for emails from Chris (from_chris)
+
+    The data is stored in lists and packed away in pickle files at the end.
+"""
+
+
+from_sara  = open("from_sara.txt", "r")
+from_chris = open("from_chris.txt", "r")
+
+from_data = []
+word_data = []
+
+### temp_counter is a way to speed up the development--there are
+### thousands of emails from Sara and Chris, so running over all of them
+### can take a long time
+### temp_counter helps you only look at the first 200 emails in the list so you
+### can iterate your modifications quicker
+temp_counter = 0
+
+
+for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+    for path in from_person:
+        ### only look at first 200 emails when developing
+        ### once everything is working, remove this line to run over full dataset
+        #temp_counter += 1
+        if temp_counter < 200:
+            path = os.path.join('..', path[:-1])
+            print path
+            email = open(path, "r")
+
+            ### use parseOutText to extract the text from the opened email
+            email_text = parseOutText(email)
+            replace_words = ['sara','shackleton','chris','germani']
+
+            ### use str.replace() to remove any instances of the words
+            ### ["sara", "shackleton", "chris", "germani"]
+            for word in replace_words:
+                email_text = email_text.replace(word,'')
+
+            ### append the text to word_data
+            word_data.append(email_text)
+
+            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'Sara':
+                from_data.appned(0)
+            elif name == 'Chris':
+                from_data.append(1)
+
+            email.close()
+
+print "emails processed"
+print word_data[152] # -> tjonesnsf stephani and sam need nymex calendar
+from_sara.close()
+from_chris.close()
+
+pickle.dump( word_data, open("your_word_data.pkl", "w") )
+pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+
+
+### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words = 'english', lowercase = True)
+vectorizer.fit_transform(word_data)
+
+feature_names = vectorizer.get_feature_names()
+print(len(feature_names)) # -> 38757
+print(feature_names[34597]) #-> stephaniethank
+
+
+
+
